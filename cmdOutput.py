2410b8a86058607dc456931947a5e2973e34c692
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2020-02-16 19:37:06 +0530

    Add files via upload

--- cmdOutput.py
+++ cmdOutput.py
@@ -1,11 +1,38 @@
 import pandas as pd
 import numpy as np
 
-#Selecting only random food from list until now
-def menu(df):
-    x=np.random.randint(df[0].size)
-    print(df[0,x])
+def match(df):
+    y,z=-1,-1
+    x=np.random.randint(df.size)
+    y=np.random.randint(df.size)
+    z=np.random.randint(df.size)
+    while y==x:
+        y=np.random.randint(df.size)
+    while z==x or z==y:
+        z=np.random.randint(df.size)
+    return [x,y,z]
 
+def additional(days,i):
+    return (k for k in [x for x in list(set(range(df[0].size))-set(days[i]))] if k not in days[i-1])
+
+#To display Menu
+def menu(df):
+    days=dict()
+    x=np.random.randint(df[0].size)
+    days[0]=match(df[0])
+    for i in range(1,7):
+        days[i]=[x for x in match(df[0]) if x not in days[i-1]]
+        while len(days[i])!=3:
+            x=additional(days,i)
+            days[i].append(x.__next__())
+    week_days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    for i in range(7):
+        print(week_days[i],end=': ')
+        for j in days[i]:
+            print('"'+df[0,j]+'"',end=' ')
+        print()
+            
+  
 #To add new food into file
 #Compares if food exists and then adds it
 def food(df):
@@ -19,7 +46,7 @@ def food(df):
     file.close()
     print('Successufully Entered')
         
-
+print('------------------------------')
 n=int(input('Press 1 to show menu or 2 to add a food item '))
 df=np.array(pd.read_csv('food.csv'))
 df=df.reshape(1,df.size)          #Converting to print result directly
@@ -27,4 +54,4 @@ if n==1:
     menu(df)
 else:
     food(df)
-
+print('------------------------------')
