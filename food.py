4e844f5da73eca1b41d46c2138f4603105fd7c49
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2020-02-20 23:47:53 +0530

    Add files via upload

--- food.py
+++ food.py
@@ -15,10 +15,12 @@ form=cgi.FieldStorage()
 for line in f:
     line=line.rstrip()
     lst.append(str(line).upper())
+lst=list(set(lst))
 f.close()
-f=open('file.txt','a')
-f.write(form['foodText'].value+'\n')
-f.close()
+if form['foodText'].value.upper() not in lst:
+    f=open('file.txt','a')
+    f.write(form['foodText'].value+'\n')
+    f.close()
 #Displaying file contents
 f=open('file.txt','r')
 print('<ul>')
