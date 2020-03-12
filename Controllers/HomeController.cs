d7526bf8037d2992b6d839631e2e883aaaf28557
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2020-03-12 15:41:23 +0530

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,15 +19,23 @@ namespace WebApplication1.Controllers
         }
         public IActionResult Menu()
         {
-            string[] food_items = new string[20];
+            string[] food_items = new string[25];
             string fileContents = System.IO.File.ReadAllText(@"C:\Users\theja\Desktop\a.txt");
             var i = 0;
             var k = 0;
             var day = 3;
+            //var food_items = new HashSet<string>(food_items1).ToArray();
             foreach (string dataLine in fileContents.Split('\n'))
             {
-                food_items[i] = dataLine;
-                i++;
+                var wave = 1;
+                for (int p = 0; p < food_items.Length; p++)
+                    if ((string.Compare(food_items[p], dataLine.ToUpper())) == 0)
+                        wave = 0;
+                if (wave == 1)
+                {
+                    food_items[i] = dataLine.ToUpper();
+                    i++;
+                }
             }
             i -= 1;
             int[] presentDay = new int[3];
@@ -63,19 +72,28 @@ namespace WebApplication1.Controllers
                 }
                 week[day++] = presentDay[2];
             }
+            var xy = 0;
             foreach (int no in week)
             {
-                foodMenu[k] = food_items[no];
+                xy++;
+                if (food_items[xy] == null)
+                    xy = 0;
+                foodMenu[k] = food_items[xy];
                 k++;
+                if (xy == food_items.Length - 1)
+                    xy = 0;
+                if (k == 22)
+                    break;
+
             }
             return View(foodMenu);
         }
         public int[] Match(string[] i)
         {
             Random rand = new Random();
-            var x = rand.Next(i.Length);
-            var y = rand.Next(i.Length);
-            var z = rand.Next(i.Length);
+            var x = 0;
+            var y = 1;
+            var z = 2;
             while (y == x)
                 y = rand.Next(i.Length);
             while (y == z || x == z)
@@ -94,27 +112,17 @@ namespace WebApplication1.Controllers
             string line = new_food;
             string fileContents = System.IO.File.ReadAllText(@"C:\Users\theja\Desktop\a.txt");
             using (StreamWriter outputFile = new StreamWriter(@"C:\Users\theja\Desktop\a.txt", true))
-                outputFile.WriteLine(line);
+                outputFile.WriteLine(line.ToUpper());
 
             return View("Food");
         }
+        //Don't touch this part
         public IActionResult Example(string Name)
         {
             UserRLModel cos = new UserRLModel();
             string root = @"C:\Users\theja\Desktop\a.txt";
-            string[] food_items = new string[20];
+            string[] food_items = new string[100];
             string fileContents = System.IO.File.ReadAllText(@"C:\Users\theja\Desktop\a.txt");
-            var i = 0;
-            foreach (string dataLine in fileContents.Split('\n'))
-            {
-                food_items[i] = dataLine;
-                i++;
-            }
-            using (StreamWriter sw = new StreamWriter(root, true))
-            { 
-                foreach(string st in food_items)
-                    sw.WriteLine(i);
-            };
             using (StreamWriter sw = new StreamWriter(root, true))
                 sw.WriteLine(Name);
             return View();
